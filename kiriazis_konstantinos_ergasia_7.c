b7892b319039b8e54b24d4a5585a98f854b84f82
KostasBuilts <78872425+KostasBuilts@users.noreply.github.com>
2025-12-09 20:51:17 +0200

    Refactor graph pathfinding code and structures

--- kiriazis_konstantinos_ergasia_7.c
+++ kiriazis_konstantinos_ergasia_7.c
@@ -1,226 +1,239 @@
-/*
- * kostas_ergasia_1.c
- * Program to find the minimum cost path between node X and node Y
- * in a graph with costs on each branch.
- * Based on F-8 program with modifications for cost calculation.
- * Author: Your Name
- */
+/* 
+Find path in graph 
 
-#include <stdio.h>
+Test data 
 
-typedef struct
-{
-    int a;
-    int b;
-    int cost;  /* New field for branch cost */
-} TBranch;
+# branches: 7 
+list of branches: 
+  0: 0 1 
+  1: 1 2 
+  2: 2 3 
+  3: 3 4 
+  4: 0 3 
+  5: 0 4 
+  6: 2 4 
+# nodes: 5 
 
-typedef struct
-{
-    int p[40];      /* Path nodes */
-    int totalCost;  /* Total cost of this path */
-    int pk;         /* Path length */
-} TPath;
+find path: 1 0 3 2 4 
+find path: 1 0 3 4 
+find path: 1 0 4 
+find path: 1 2 3 0 4 
+find path: 1 2 3 4 
+find path: 1 2 4 
+*/ 
+#include <stdio.h> 
+#include <math.h> 
 
-int findnode(int a[], int n, int x)
+
+
+
+typedef struct 
+{ 
+    int node1; 
+    int node2; 
+    int branch_weight;
+} TBranch; 
+
+typedef struct 
+{ 
+    int path_nodes[40]; 
+    int pk; 
+    int path_cost;
+} TPath; 
+
+
+void copyArray(int src[], int dst[], int len)
 {
     int i;
-    for(i = 0; i < n; i++)
+    for (i=0; i<len; i++)
     {
-        if(a[i] == x)
-        {
-            return i;
-        }
+        dst[i]=src[i];
     }
-    return -1;
 }
 
-void printarr(int a[], int n)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+int findnode( int a[], int currentNode, int x) 
+{   
+    int i; 
+    for(i=0; i<currentNode; i++) 
+    { 
+        if(a[i]==x) 
+        {            
+            return i; 
+        } 
+    }    
+    return -1; 
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+void printarr( int a[], int n) 
 {
-    int i;
-    for(i = 0; i < n; i++)
-    {
+    int i; 
+    for( i=0; i<n; i++) 
+    {   
         printf("%3d", a[i]);
-    }
+    } 
+    printf("\n"); 
 }
 
-void printPath(TPath path)
-{
-    int i;
-    printf("Path: ");
-    for(i = 0; i < path.pk; i++)
-    {
-        printf("%3d", path.p[i]);
-    }
-    printf("  | Cost: %d\n", path.totalCost);
-}
 
-void printbranches(TBranch a[], int nb)
-{
-    int i;
-    for(i = 0; i < nb; i++)
-    {
-        printf("%3d: %3d %3d (cost: %d)\n", i, a[i].a, a[i].b, a[i].cost);
-    }
-}
+///////////////////////////////////////////////////////////////////////////////////////////
+void printPath( TPath a) 
+{   
+    int i; 
+    for( i=0; i<a.pk; i++) 
+    {   
+        printf("%3d", a.path_nodes[i]);
+    } 
+    printf("\n"); 
+} 
 
-/* Global variables to track the best path found */
-TPath bestPath;
-int bestCost = 999999; /* Initialize with a very large number */
 
-void findMinPath(int current, int target, 
-                 int netw[][40], int costs[][40], 
-                 int n, int path[], int *pk, int currentCost)
-{
-    int i;
-    
-    /* Add current node to path */
-    path[*pk] = current;
-    *pk = *pk + 1;
-    
-    /* If we reached the target */
-    if(current == target)
-    {
-        /* Found a complete path */
-        TPath foundPath;
-        int j;
-        
-        /* Copy path to foundPath */
-        for(j = 0; j < *pk; j++)
-        {
-            foundPath.p[j] = path[j];
-        }
-        foundPath.pk = *pk;
-        foundPath.totalCost = currentCost;
-        
-        /* Print the found path */
-        printPath(foundPath);
-        
-        /* Check if this is better than the best so far */
-        if(currentCost < bestCost)
-        {
-            bestCost = currentCost;
-            bestPath = foundPath;
-        }
-        
-        /* Backtrack */
-        *pk = *pk - 1;
-        return;
-    }
-    
-    /* Try all adjacent nodes */
-    for(i = 0; i < n; i++)
-    {
-        if(netw[current][i] != 0)
-        {
-            /* Check if node i is not already in the path */
-            if(findnode(path, *pk, i) == -1)
-            {
-                /* Add the cost of this branch */
-                int newCost = currentCost + costs[current][i];
-                
-                /* Only continue if this path could be better than the best so far */
-                if(newCost < bestCost)
-                {
-                    findMinPath(i, target, netw, costs, n, path, pk, newCost);
-                }
-            }
-        }
-    }
-    
-    /* Backtrack */
-    *pk = *pk - 1;
-}
+///////////////////////////////////////////////////////////////////////////////////////////
+void printbranches( TBranch branches[], int nb) 
+{   
+    int i; 
+    for( i=0; i<nb; i++) 
+    {   
+        printf("%3d: %3d %3d  weight= %d\n", i, branches[i].node1, branches[i].node2, branches[i].branch_weight); 
+    } 
+} 
 
-int main(void)
-{
-    TBranch a[40];
-    int netw[40][40] = {0};
-    int costs[40][40] = {0}; /* New matrix for costs */
-    int path[40];
-    int pk;
-    int i, nb, nn, maxn;
-    int startNode, endNode;
-    
-    /* Test data with costs */
-    nb = 9; /* 9 branches for better testing */
-    a[0].a = 0; a[0].b = 1; a[0].cost = 2;
-    a[1].a = 1; a[1].b = 2; a[1].cost = 3;
-    a[2].a = 2; a[2].b = 3; a[2].cost = 1;
-    a[3].a = 3; a[3].b = 4; a[3].cost = 4;
-    a[4].a = 0; a[4].b = 3; a[4].cost = 7;
-    a[5].a = 0; a[5].b = 4; a[5].cost = 8;
-    a[6].a = 2; a[6].b = 4; a[6].cost = 2;
-    a[7].a = 1; a[7].b = 3; a[7].cost = 5; /* Additional branch */
-    a[8].a = 1; a[8].b = 4; a[8].cost = 6; /* Additional branch */
-    
-    printf("\n");
-    printf("===========================================\n");
-    printf("MINIMUM COST PATH FINDER\n");
-    printf("Based on F-8 program with cost extensions\n");
-    printf("===========================================\n\n");
-    
-    printf("Processing graph data...\n");
-    printf("# branches: %d\n", nb);
-    printf("List of branches with costs:\n");
-    printbranches(a, nb);
-    
-    /* Build adjacency matrix and cost matrix */
-    maxn = 0;
-    for(i = 0; i < nb; i++)
-    {
-        netw[a[i].a][a[i].b] = 1;
-        netw[a[i].b][a[i].a] = 1;
-        costs[a[i].a][a[i].b] = a[i].cost;
-        costs[a[i].b][a[i].a] = a[i].cost;
-        
-        if(a[i].a > maxn) maxn = a[i].a;
-        if(a[i].b > maxn) maxn = a[i].b;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+void findpath( int startNode, int endNode, int con_arr[][40], int numberOfNodes, int p_arr[], int *pk, int *p_idx, TPath path[]) 
+{    
+    int i; 
+    p_arr[*pk]=startNode; 
+    *pk=*pk+1; 
+    if (startNode==endNode) 
+    { 
+        copyArray(p_arr, path[*p_idx].path_nodes, *pk); // apothikeuo ta nodes tou path stin structure
+        path[*p_idx].pk=*pk;                            // apothikeuo to plithos ton nodes
+
+        printf("find path: ");  printarr( p_arr, *pk); 
+        *p_idx = *p_idx + 1;
+        return; 
+    } 
+
+    for(i=0; i<numberOfNodes; i++) 
+    { 
+        if (con_arr[startNode][i] > 0) 
+        { 
+            if (findnode( p_arr, *pk, i) == -1 ) 
+            { 
+                findpath( i, endNode, con_arr, numberOfNodes, p_arr, pk, p_idx, path); 
+                *pk= *pk - 1; 
+            } 
+        } 
     }
-    
-    nn = maxn + 1;
-    printf("# nodes: %d\n", nn);
-    
-    /* Reset best path variables */
-    bestCost = 999999;
-    bestPath.pk = 0;
-    
-    /* Find minimum cost path from node 1 to node 4 */
-    startNode = 1;
-    endNode = 4;
-    
-    printf("\n");
-    printf("===========================================\n");
-    printf("Finding all paths from node %d to node %d:\n", startNode, endNode);
-    printf("===========================================\n");
-    
-    pk = 0;
-    findMinPath(startNode, endNode, netw, costs, nn, path, &pk, 0);
-    
-    printf("\n");
-    printf("===========================================\n");
-    printf("RESULT:\n");
-    printf("===========================================\n");
-    
-    if(bestPath.pk > 0)
-    {
-        printf("Minimum cost path found!\n");
-        printf("Path: ");
-        for(i = 0; i < bestPath.pk; i++)
+     
+} 
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+int main(void) 
+{ 
+    TBranch myBranches[40]; 
+    int connection_array[40][40]={0}; 
+    int path_array[40]; 
+    int pk; 
+    int i, num_of_branches, num_of_nodes, maxn, path_idx;
+    TPath myPaths[40];
+
+    /* 
+    //read graph 
+    printf("enter # branches: "); 
+    scanf("%d",&num_of_branches); 
+
+    for(i=0; i<num_of_branches; i++) 
+    {   
+        printf("enter branch  %d:\n",i); 
+        scanf("%d",&myBranches[i].node1); 
+        scanf("%d",&myBranches[i].node2); 
+        scanf("%d",&myBranches[i].branch_weight); 
+    } 
+    */ 
+    /**/  
+    //test graph 
+    num_of_branches=7; 
+    myBranches[0].node1=0; 
+    myBranches[0].node2=1; 
+    myBranches[0].branch_weight=6;
+    myBranches[1].node1=1; 
+    myBranches[1].node2=2; 
+    myBranches[1].branch_weight=9;
+    myBranches[2].node1=2; 
+    myBranches[2].node2=3; 
+    myBranches[2].branch_weight=4;
+    myBranches[3].node1=3; 
+    myBranches[3].node2=4; 
+    myBranches[3].branch_weight=7;
+    myBranches[4].node1=0; 
+    myBranches[4].node2=3;
+    myBranches[4].branch_weight=2;   
+    myBranches[5].node1=0; 
+    myBranches[5].node2=4;
+    myBranches[5].branch_weight=4;   
+    myBranches[6].node1=2; 
+    myBranches[6].node2=4; 
+    myBranches[6].branch_weight=1;
+    /**/ 
+    printf("\n\n"); 
+    printf("Processing ..\n"); 
+    printf("# branches: %d\n", num_of_branches); 
+    printf("list of branches:\n"); 
+    printbranches( myBranches, num_of_branches); 
+    maxn=0; 
+
+    // dimioyrgia Network Array
+    for(i=0; i<num_of_branches; i++) 
+    {   
+        if (myBranches[i].branch_weight<=0) // check for illegal weights
         {
-            printf("%d", bestPath.p[i]);
-            if(i < bestPath.pk - 1)
-            {
-                printf(" -> ");
-            }
+            printf("\n*********************** WARNING ************************\n");
+            printf("The branch %d<->%d has weight=%d (the branch is ingnored)\n", myBranches[i].node1, myBranches[i].node2, myBranches[i].branch_weight);
+            printf("********************************************************\n");
         }
-        printf("\nTotal cost: %d\n", bestCost);
-    }
-    else
-    {
-        printf("No path found between node %d and node %d\n", startNode, endNode);
-    }
+
+        //tranform input branches into 2 dimension connection network array netw[][]
+        connection_array[myBranches[i].node1][myBranches[i].node2]= myBranches[i].branch_weight; 
+        connection_array[myBranches[i].node2][myBranches[i].node1]= myBranches[i].branch_weight; 
+        if (myBranches[i].node1 > maxn) 
+            maxn=myBranches[i].node1;
+        if (myBranches[i].node2 > maxn) 
+            maxn=myBranches[i].node2; 
+    } 
+    num_of_nodes = maxn+1; 
+    printf("# nodes: %d\n\n", num_of_nodes); 
     
+    //find path from node 1 to node 4 
+    pk=0; path_idx=0;
+    findpath( 1, 4, connection_array, num_of_nodes, path_array, &pk, &path_idx, myPaths);  
+
+    // calculate total_costs per path
+    for(i=0; i<path_idx; i++)
+    {
+        for(int j=0; j<myPaths[i].pk-1; j++)  //for each path iterate all nodes
+        {
+            myPaths[i].path_cost =  + myPaths[i].path_cost + connection_array[myPaths[i].path_nodes[j]][myPaths[i].path_nodes[j+1]];
+        }
+    }
+
+
+    printf("\n---------------------------- RESULTS -----------------------------\n");
+    for(i=0; i<path_idx; i++)
+    {
+        printf("pathNo=%2d cost=%3d  nodeCount=%2d  nodeArray=", i, myPaths[i].path_cost, myPaths[i].pk );
+        printPath(myPaths[i]);
+    }
+
+
+
     printf("\n");
-    return 0;
+    return 0; 
 }
